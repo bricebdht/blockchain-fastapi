08ec2541dd0dbabd3f0b07d90b375f98ae77a410
Brice Boudehent <brice.boudehent@gmail.com>
2021-09-26 15:20:31 +0200

    adding model

--- app/routers/transactions.py
+++ app/routers/transactions.py
@@ -1,8 +1,9 @@
+from app.models.transaction_model import Transaction
 from fastapi import APIRouter
 
 router = APIRouter()
 
 
-@router.post("/transactions/new", methods=["POST"])
-def post() -> None:
+@router.post("/transactions/new")
+def post(transaction: Transaction) -> None:
     return "Add transaction"
