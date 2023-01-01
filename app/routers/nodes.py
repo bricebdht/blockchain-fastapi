33b6d9041aec54ebd499d6f92c1995fabbbf2bea
Brice Boudehent <brice.boudehent@untienots.com>
2023-01-01 23:45:51 +0100

    adding resolve endpoint

--- app/routers/nodes.py
+++ app/routers/nodes.py
@@ -1,10 +1,14 @@
 """
 APIs related to nodes
 """
+from typing import List
+
+import requests
 from fastapi import APIRouter
 
-from app.models.block_model import Block, Transaction
+from app.models.block_model import Block
 from app.models.node_model import Node
+from app.routers.mine import validate_proof
 
 router = APIRouter(
     prefix="/nodes",
@@ -23,3 +27,37 @@ def post(node_data: Node) -> str:
     new_node = Node(index=new_index, address=node_data.address)
     new_node.insert()
     return "Node has been registered"
+
+
+def validate_chain(chain: List[Block]):
+    last_block = Block.get_last_block()
+    for i in range(1, len(chain)):
+        block = chain[i]
+        if block["previous_hash"] != Block(**last_block).hash():
+            return False
+        if not validate_proof(last_proof=last_block["proof"], proof=block["proof"]):
+            return False
+        last_block = block
+    return True
+
+
+@router.post("/resolve")
+def get() -> str:
+    """
+    Resolve conflicts with the chains of other nodes
+    The longest chain is considered the right one
+    """
+    # Get chains from other nodes
+    nodes = Node.get_collection().find()
+    current_length = Block.get_collection().count_documents({})
+    for node in nodes:
+        response = requests.get(f"{node.address}/chain")
+        length = response["length"]
+        chain = response["chain"]
+        if length > current_length and validate_chain(chain=chain):
+            for i in range(current_length, length):
+                new_block = Block(**chain[i])
+                new_block.insert()
+            return "The blockain has been updated"
+
+    return "The blockchain is already up to date"
